91970e94c3317a88ebb73d5e09dab9ec5a7e525d
Your Name <you@example.com>
2023-12-01 19:17:27 +0300

    7lab some shit

--- code/lab7/main.cpp
+++ code/lab7/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <iomanip>
 #include <string.h>
+
+struct Symbol{
+    unsigned char sym;
+    unsigned char encoded; 
+    int cnt = 0;
+    int encoded_cnt = 0;
+};
 
 std::ifstream::pos_type filesize(const char* filename)
 {
@@ -43,13 +51,13 @@ int* caesar_shifts(const char* filename, int length){
     int* res = new int[length];
 
     int idx = 0;
-    char* s = new char[50];
+    unsigned char* s = new unsigned char[50];
     while(!fin.eof()){
         fin >> s;
 
-        for(int i = 0; i < strlen(s); ++i){
+        for(int i = 0; i < strlen((char*)s); ++i){
             res[idx] += s[i];
-            res[idx] %= 128;
+            res[idx] %= 32;
         }
 
         idx++;
@@ -64,9 +72,9 @@ void encode(int* shifts, int shifts_cnt, const char* inputfile, const char* outp
     std::ifstream fin(inputfile);
     std::ofstream fout(outputfile);
 
-    int idx = 0; char c;
+    int idx = 0; unsigned char c;
     while(fin >> c){
-        char key = moduled(c + shifts[idx], -127, 127);
+        unsigned char key = moduled(c + shifts[idx], 0, 255);
         fout << key;
 
         idx++; idx = idx % shifts_cnt;
@@ -79,15 +87,61 @@ void decode(int* shifts, int shifts_cnt, const char* inputfile, const char* outp
     std::ifstream fin(inputfile);
     std::ofstream fout(outputfile);
  
-    int idx = 0; char c;
+    int idx = 0; unsigned char c;
     while(fin >> c){      
-        char key = moduled(c - shifts[idx], -127, 127);
+        unsigned char key = moduled(c - shifts[idx], 0, 255);
         fout << key;
 
         idx++; idx = idx % shifts_cnt;
     }
 
     fin.close(); fout.close();
+}
+
+Symbol* creating_table(const char* sourcefile, const char* encodedfile){
+    std::ifstream fenc(encodedfile);
+    std::ifstream fdec(sourcefile);
+
+    Symbol* stat = new Symbol[256];
+    unsigned char enc, dec;
+    while(fenc >> enc && fdec >> dec){
+        stat[dec].cnt++;
+        stat[enc].encoded_cnt++;
+
+        if(stat[dec].cnt == 1){
+            stat[dec].sym = dec;
+            stat[dec].encoded = enc;
+        }
+    }
+
+    fenc.close(); fdec.close();
+
+    return stat;
+}
+
+void print_table(Symbol* stat, const char* outputfile){
+    std::ofstream fout(outputfile);
+
+    fout << "|   символ   |" << std::setw(10);
+    fout << "|   кол-во в исх-ом   |" << std::setw(10);
+    fout << "|   кол-во в вар-ов   |" << std::setw(10);
+    fout << "|   размер блокнота   |" << std::setw(10);
+    fout << "|   длина исх-ый   |" << std::setw(10);
+    fout << '\n';
+
+    for(int i = 0; i < 256; ++i){
+        if(stat[i].cnt > 0 && stat[i].sym){
+            std::cout << stat[i].sym << std::endl;
+            fout << "|" << stat[i].sym << "           |";
+            fout << "|" << stat[i].cnt << "                    |";
+            fout << "|" << stat[i].encoded_cnt << "                    |";
+            fout << "|" << filesize("key.txt") << "                  |"; 
+            fout << "|" << filesize("source.txt") << "                |"; 
+            fout << '\n';
+        }
+    }
+
+    fout.close();
 }
 
 int main(){
@@ -100,6 +154,12 @@ int main(){
     decode(shifts, words_cnt("key.txt"), "encoded.txt", "decoded.txt");
 
     delete []shifts;
+
+
+    Symbol* stat = creating_table("source.txt", "encoded.txt");
+    print_table(stat, "table.txt");
+
+    delete []stat;
 
     return 0;
 }
--- code/lab7/test.cpp
+++ code/lab7/test.cpp
@@ -2,27 +2,12 @@
 #include <fstream>
 #include <string.h>
 
-int moduled(int n, int l, int r){
-    if(n >= l && n <= r) return n;
-
-    if(n < l){
-        return r - abs(l - n) % abs(r - l);
-    }
-
-    return l + abs(r - n) % abs(r - l);
-}
-
 
 int main(){
-    std::ofstream t1("t2.txt");
-    std::ifstream t2("t2.txt");
+    std::ofstream t1("t1.txt");
 
-    char c;
-    while(t2 >> c){
-        t1 << (char)(c - 28);
-    }
-
-    t1.close(); t2.close();
+    t1 << 'a';
+    t1.close();
 
     return 0;
 }
